d3885f47703a6ae92683b619305425cb5ed3dbf6
maboiteinformatique <maboiteinformatique@gmail.com>
2022-02-22 11:07:08 +0000

    good place

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hole que tal?'));
+app.get('/', (req, res) => res.send('c'est bien good place'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
