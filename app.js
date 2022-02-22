428a5922c4341caac9ecab466471e7a26301ae35
maboiteinformatique <maboiteinformatique@gmail.com>
2022-02-22 10:21:13 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('autre chose'));
+app.get('/', (req, res) => res.send('good place, c'est bien'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
